c0483016a377130eefdad1fefe39de0f56c96205
Adam <adamlederer@live.ca>
2023-03-22 23:08:20 -0600

    Made minimap in profile for testing. Make sure that my pin page had a minimap.

--- src/components/PinPage.vue
+++ src/components/PinPage.vue
@@ -1,6 +1,9 @@
 <template>
     <v-container>
         <h1>Pins</h1>
+        <div>
+            <MapPage/>
+        </div>
             <h3>Title:</h3>
                 <v-text-field
                 v-model="title"
@@ -27,10 +30,22 @@
 
 <script>
 import FooterBar from "../components/FooterBar.vue";
+import MapPage from "../components/VueLeaflet.vue"
     export default {
         name: 'PinPage',
         components: {
-            FooterBar
+            FooterBar,
+            MapPage,
     },
     }
 </script>
+
+<style lang="scss" scoped>
+.map{
+    display: grid;
+    // width: 30vw;
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+}
+</style>
